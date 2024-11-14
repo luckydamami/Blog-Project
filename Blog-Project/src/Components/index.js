443d6fdad3ef71954267ab7c59d709a7bcb87434
lucky <luckydamamii2410@gmail.com>
2024-11-14 13:11:05 +0530

    Mega project of complex blog

---
+++ Blog-Project/src/Components/index.js
@@ -0,0 +1,32 @@
+//Import and Export Multiple Components in onefile
+import Header from "./Header/Header";
+import Select from "./Select";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Button from "./Button";
+import Input from "../Components/Input";
+import RTE from "./RTE";
+import Signup from "../Components/Signup";
+import Login from "./Login";
+import PostForm from "./Post-Form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  Button,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Input,
+  Select,
+};
